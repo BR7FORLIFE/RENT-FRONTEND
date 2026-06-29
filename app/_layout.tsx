2c66cc5bec445d4c9ba6243e3fd521380dff8a7f
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-29 12:12:09 -0500

    feat: implement splash screen and toast notifications in AppLayout; update index redirection based on authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,25 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import Toast from "react-native-toast-message";
+import SplashScreen from "../components/splash-screen";
 import { queryClient } from "../core/tanstackconfig/config";
+import { useBootstrapApp } from "../hooks/hooks";
 
 export default function AppLayout() {
+  const isLoading = useBootstrapApp();
+
   const [loaded] = useFonts({
     arimo: require("../assets/fonts/arimo.ttf"),
   });
 
   if (!loaded) {
     return null;
+  }
+
+  if (isLoading) {
+    //Mandamos el splashScreen
+    return <SplashScreen />;
   }
 
   return (
@@ -19,6 +29,7 @@ export default function AppLayout() {
           headerShown: false,
         }}
       />
+      <Toast position="top" visibilityTime={2000} />
     </QueryClientProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
 import { Redirect } from "expo-router";
+import { useAuth } from "../stores/auth-store";
 
 export default function Index() {
-  return <Redirect href="/register" />;
+  const { accessToken } = useAuth();
+  return (
+    <Redirect
+      href={accessToken ? "/home/(tabs)/properties-services" : "/login"}
+    />
+  );
 }
