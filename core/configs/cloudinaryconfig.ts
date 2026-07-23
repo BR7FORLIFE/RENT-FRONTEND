a206e96853bd1fa71c6268571df0b1946418af7a
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-07-23 13:18:05 -0500

    feat: refactor Cloudinary integration and extend CloudinaryResponse type

--- core/configs/cloudinaryconfig.ts
+++ core/configs/cloudinaryconfig.ts
@@ -1,14 +1,27 @@
-import { Cloudinary } from "@cloudinary/url-gen";
-import {
-    CloudinaryApiKey,
-    CloudinaryApiSecret,
-    CloudinaryCloudName,
-} from "../env";
+import type { CloudinaryResponse } from "../../features/property-registration/types";
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_PRESET_NAME } from "../env";
 
-export const cloudinaryClient = new Cloudinary({
-    cloud: {
-        cloudName: CloudinaryCloudName,
-        apiKey: CloudinaryApiKey,
-        apiSecret: CloudinaryApiSecret,
-    },
-});
+export async function uploadImage(imgUri: string) {
+    console.log({ CLOUDINARY_CLOUD_NAME, CLOUDINARY_PRESET_NAME });
+    const CloudinaryUrlCloud = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
+
+    const data = new FormData();
+    data.append("upload_preset", CLOUDINARY_PRESET_NAME!);
+    data.append("file", {
+        uri: imgUri,
+        name: `image-${Date.now()}.jpg`,
+        type: "image/jpeg",
+    } as any);
+
+    const req = await fetch(CloudinaryUrlCloud, { method: "POST", body: data });
+
+    if (!req.ok) {
+        const error = await req.json();
+
+        throw new Error(
+            error.error?.message ?? "Error al cargar la imagen en cloudinary",
+        );
+    }
+
+    return (await req.json()) as CloudinaryResponse;
+}
--- features/property-registration/types.ts
+++ features/property-registration/types.ts
@@ -27,3 +27,36 @@ export interface IAPropertyRegistrationResponse {
     name: string;
     description: string;
 }
+
+//cloudinary response
+export interface CloudinaryResponse {
+    asset_id: string;
+    public_id: string;
+    version: number;
+    version_id: string;
+    signature: string;
+    width: number;
+    height: number;
+    format: string;
+    resource_type: string;
+    created_at: string;
+    tags: any[];
+    bytes: number;
+    type: string;
+    placeholder: boolean;
+    url: string;
+    secure_url: string;
+    asset_folder: string;
+    display_name: string;
+    eager: Eager[];
+}
+
+export interface Eager {
+    transformation: string;
+    width: number;
+    height: number;
+    bytes: number;
+    format: string;
+    url: string;
+    secure_url: string;
+}
