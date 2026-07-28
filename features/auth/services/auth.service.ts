9b29bdb135e3ac241034e1c36ef95c3c7d57e239
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-07-27 20:55:45 -0500

    feat: enhance validation logic in auth service; add regex checks for email and phone number; improve empty field handling

--- features/auth/services/auth.service.ts
+++ features/auth/services/auth.service.ts
@@ -7,12 +7,89 @@ import {
 import { useAuth } from "../../../stores/auth-store";
 import type { Storage } from "../../../types/global";
 
-export const fieldsNotValid = (data: RegisterType): string[] => {
+const TRIM_TEXT_REGEX = /\s+/g;
+
+const EMAIL_REGEX_VALIDATION =
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const NUMBER_REGEX_VALIDATION = /^[0-9]+$/;
+
+interface ValidateContentDataInt {
+    emptyFields: string[] | null;
+    contentResult: RegisterType | null;
+    invalidFields: string[] | null;
+}
+
+const fieldsNotValid = (data: RegisterType): string[] => {
     const notValidField = Object.entries(data)
         .filter(([_, value]) => value.trim().length === 0)
         .map(([key, _]) => key);
 
     return notValidField;
+};
+
+const validateWithRegex = (text: string, regex: RegExp) => {
+    return regex.test(text);
+};
+
+export const validateContentData = (
+    data: RegisterType,
+): ValidateContentDataInt => {
+    //validar los campos no rellenados y notificar a la UI
+    const emptyFields = fieldsNotValid(data);
+
+    if (emptyFields.length !== 0) {
+        //campos a enviar a la UI para notificar
+        console.log({ emptyFields });
+        return { emptyFields, contentResult: null, invalidFields: null };
+    }
+
+    //proceso de validacion de los datos del formulario
+    const result: RegisterType = {
+        username: "", // listo
+        password: "", // listo
+        email: "", // listo
+        cellphone: "", // listo
+        fullname: "", //listo
+        identificationNumber: "",
+        identificationType: data.identificationType,
+    };
+
+    //transformamos y tratamos las variables de tezto como username fullname y password
+    result["username"] = data.username.replace(TRIM_TEXT_REGEX, "");
+    result["fullname"] = data.fullname.replace(TRIM_TEXT_REGEX, "");
+    result["password"] = data.password.replace(TRIM_TEXT_REGEX, "");
+
+    //validamos el email con su respectivo regex
+    if (!validateWithRegex(data.email, EMAIL_REGEX_VALIDATION)) {
+        //notificamos que el email esta mal estructurado
+        return {
+            contentResult: null,
+            emptyFields: null,
+            invalidFields: ["EMAIL"],
+        };
+    }
+
+    result["email"] = data.email; // asignamos el email ya que validamos que es correcto
+
+    if (
+        !validateWithRegex(data.cellphone, NUMBER_REGEX_VALIDATION) ||
+        !validateWithRegex(data.identificationNumber, NUMBER_REGEX_VALIDATION)
+    ) {
+        //notificamos que el telefono no tiene el formato esperado
+        // o por consecuencia el numero de identificacion
+        return {
+            contentResult: null,
+            emptyFields: null,
+            invalidFields: ["CELLPHONE", "IDENTIFICATION_NUMBER"],
+        };
+    }
+
+    result["cellphone"] = data.cellphone;
+    result["identificationNumber"] = data.identificationNumber;
+
+    console.log({ result });
+    return { contentResult: result, emptyFields: null, invalidFields: null };
 };
 
 export const validateRegisterFields = (data: RegisterType) => {
@@ -47,7 +124,6 @@ export function InfoStorage(): Storage<{
                 refreshToken: data.refreshToken,
             }),
         };
-
         await AsyncStorage.setItem(KEY_STORAGE, JSON.stringify(updated));
     };
 
